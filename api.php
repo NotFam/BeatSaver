9b59b31ed5e0d65e93ad5d5f6e04c484450dc5a5
root <root@beatsaver.com>
2018-05-08 20:46:48 +0000

    added API!

---
+++ api.php
@@ -0,0 +1,81 @@
+<?php
+require("config.php");
+header('Content-Type: application/json');
+$offset = (int) @$_GET["off"];
+
+if(@$_GET['mode'] = 'top'){
+$bt = $database->select("beats", [
+        "id",
+        "beatname",
+        "ownerid",
+        "downloads",
+        "upvotes",
+        "beattext",
+        "uploadtime",
+        "songName",
+        "songSubName",
+        "authorName",
+        "beatsPerMinute",
+        "difficultyLevels",
+        "img"
+], [
+        'LIMIT' => [$offset, 15],
+        "ORDER" => ["downloads" => "DESC",]
+]);
+foreach($bt as $key => $row){
+$bt[$key]["difficultyLevels"] = json_decode($bt[$key]["difficultyLevels"]);
+}
+echo json_encode($bt, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_PRETTY_PRINT);
+}
+
+if(@$_GET['mode'] = 'new'){
+$bt = $database->select("beats", [
+        "id",
+        "beatname",
+        "ownerid",
+        "downloads",
+        "upvotes",
+        "beattext",
+        "uploadtime",
+        "songName",
+        "songSubName",
+        "authorName",
+        "beatsPerMinute",
+        "difficultyLevels",
+        "img"
+], [
+        'LIMIT' => [$offset, 15],
+        "ORDER" => ["id" => "DESC",]
+]);
+foreach($bt as $key => $row){
+$bt[$key]["difficultyLevels"] = json_decode($bt[$key]["difficultyLevels"]);
+}
+
+echo json_encode($bt, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_PRETTY_PRINT);
+}
+
+if(@$_GET['mode'] = 'star'){
+$bt = $database->select("beats", [
+        "id",
+        "beatname",
+        "ownerid",
+        "downloads",
+        "upvotes",
+        "beattext",
+        "uploadtime",
+        "songName",
+        "songSubName",
+        "authorName",
+        "beatsPerMinute",
+        "difficultyLevels",
+        "img"
+], [
+        'LIMIT' => [$offset, 15],
+        "ORDER" => ["upvotes" => "DESC",]
+]);
+foreach($bt as $key => $row){
+$bt[$key]["difficultyLevels"] = json_decode($bt[$key]["difficultyLevels"]);
+}
+echo json_encode($bt, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_PRETTY_PRINT);
+}
+
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=1024">
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+<!-- Hacker Secret API https://beatsaver.com/api.php?mode=top&off=0 | https://beatsaver.com/api.php?mode=star&off=0 | https://beatsaver.com/api.php?mode=new&off=0 -->
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
